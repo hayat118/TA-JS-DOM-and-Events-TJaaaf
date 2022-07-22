ec0b968a9e071da24570f2fe6a3979d4710cbb36
ravi11o <ravi.suraj110@gmail.com>
2022-07-22 15:47:36 +0530

    memory game

--- block-BJaaex/code/script.js
+++ block-BJaaex/code/script.js
@@ -1,49 +1,109 @@
-let allBoxes=document.querySelectorAll(".box");
-let box=document.querySelector(".box")
 
 
+'use strict';
 
+var cardsArray = [{
+  'name': 'shell',
+  'img': 'img/blueshell.png'
+}, {
+  'name': 'star',
+  'img': 'img/star.png'
+}, {
+  'name': 'bobomb',
+  'img': 'img/bobomb.png'
+}, {
+  'name': 'mario',
+  'img': 'img/mario.png'
+}, {
+  'name': 'luigi',
+  'img': 'img/luigi.png'
+}, {
+  'name': 'peach',
+  'img': 'img/peach.png'
+}, {
+  'name': '1up',
+  'img': 'img/1up.png'
+}, {
+  'name': 'mushroom',
+  'img': 'img/mushroom.png'
+}, {
+  'name': 'thwomp',
+  'img': 'img/thwomp.png'
+}, {
+  'name': 'bulletbill',
+  'img': 'img/bulletbill.png'
+}, {
+  'name': 'coin',
+  'img': 'img/coin.png'
+}, {
+  'name': 'goomba',
+  'img': 'img/goomba.png'
+}];
 
-
-
-
-allBoxes.forEach((box,index)=>{
-
-  box.addEventListener("click",(event)=>{
-
-    // box.innerHTML="";
-
-
-  //   for(let i=0; i<=16; i++){
-  //    let box=document.querySelector(".box")
-  //    let randomNO=getRandomNumber(16);
-  //    box.innerText=randomNO;
-  //  }
-      // event.target.innerText="";
-
-   setTimeout(() => {
-      event.target.innerText="";
-      
-    }, 2000);
-
-  })
+var gameGrid = cardsArray.concat(cardsArray).sort(function () {
+  return 0.5 - Math.random();
 });
 
+var firstGuess = '';
+var secondGuess = '';
+var count = 0;
+var previousTarget = null;
+var delay = 1200;
 
-box.addEventListener('click', event => {
+var game = document.getElementById('game');
+var grid = document.createElement('section');
+grid.setAttribute('class', 'grid');
+game.appendChild(grid);
 
-  const clicked = event.target;
+gameGrid.forEach(function (item) {
+  var name = item.name,
+      img = item.img;
 
-  if (
-    clicked.nodeName === 'SECTION' ||
-    clicked === previousTarget ||
-    clicked.parentNode.classList.contains('selected') ||
-    clicked.parentNode.classList.contains('match')
-  ) {
+
+  var card = document.createElement('div');
+  card.classList.add('card');
+  card.dataset.name = name;
+
+  var front = document.createElement('div');
+  front.classList.add('front');
+
+  var back = document.createElement('div');
+  back.classList.add('back');
+  back.style.backgroundImage = 'url(' + img + ')';
+
+  grid.appendChild(card);
+  card.appendChild(front);
+  card.appendChild(back);
+});
+
+var match = function match() {
+  var selected = document.querySelectorAll('.selected');
+  selected.forEach(function (card) {
+    card.classList.add('match');
+  });
+};
+
+var resetGuesses = function resetGuesses() {
+  firstGuess = '';
+  secondGuess = '';
+  count = 0;
+  previousTarget = null;
+
+  var selected = document.querySelectorAll('.selected');
+  selected.forEach(function (card) {
+    card.classList.remove('selected');
+  });
+};
+
+grid.addEventListener('click', function (event) {
+
+  var clicked = event.target;
+
+  if (clicked.nodeName === 'SECTION' || clicked === previousTarget || clicked.parentNode.classList.contains('selected') || clicked.parentNode.classList.contains('match')) {
     return;
   }
 
-if (count < 2) {
+  if (count < 2) {
     count++;
     if (count === 1) {
       firstGuess = clicked.parentNode.dataset.name;
@@ -63,16 +123,4 @@ if (count < 2) {
     }
     previousTarget = clicked;
   }
-
 });
- 
-
-
-// function getRandomNumber(max){
-//   return Math.floor(Math.random()*max)
-// }
-
-
-
-// allBoxes.addEventListener("click",handleclick);
-
